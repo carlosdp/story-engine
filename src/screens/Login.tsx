556c29cc7ec36dfaccba23452ae6885feb707e9c
Carlos Diaz-Padron <carlosdiazpadron@gmail.com>
2023-04-22 18:16:06 -0400

    make login screen nicer (#6)

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,4 +1,4 @@
-import { Button, Center } from '@chakra-ui/react';
+import { Button, Center, Flex, Heading, Text } from '@chakra-ui/react';
 import { useCallback } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 
@@ -19,9 +19,18 @@ export const Login = () => {
 
   return (
     <PageContainer>
-      <Center>
-        <Button onClick={onDiscordLogin}>Login with Discord</Button>
-      </Center>
+      <Center></Center>
+      <Flex alignItems="center" justifyContent="center" flexDirection="column" flex={1}>
+        <Flex alignItems="center" flexDirection="column" gap="32px" minHeight="400px">
+          <Flex alignItems="center" flexDirection="column" gap="32px">
+            <Heading size="4xl">Welcome</Heading>
+            <Text fontSize="lg">
+              Let's get you registered for <strong>Whispering Fable!</strong>
+            </Text>
+            <Button onClick={onDiscordLogin}>Login with Discord</Button>
+          </Flex>
+        </Flex>
+      </Flex>
     </PageContainer>
   );
 };
