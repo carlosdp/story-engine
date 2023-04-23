cd1dd25aa1b89c4b0bbbce4088d28df20ae97a73
Carlos Diaz-Padron <carlosdiazpadron@gmail.com>
2023-04-23 13:18:19 -0400

    add sentry to frontend (#8)

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,6 @@
 import { ChakraProvider, ColorModeScript, GlobalStyle } from '@chakra-ui/react';
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/react';
 import { SupabaseClient } from '@supabase/supabase-js';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -9,6 +11,18 @@ import App from './App';
 import { SupabaseProvider } from './SupabaseProvider';
 import { Database } from './supabaseTypes';
 import { theme } from './theme';
+
+if (import.meta.env.MODE === 'production') {
+  Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN,
+    integrations: [new BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1,
+  });
+}
 
 const client = new SupabaseClient<Database>(
   import.meta.env.DEV ? `${window.location.origin}/supabase` : import.meta.env.VITE_SUPABASE_URL!,
