0ac49ab03aafa78e71be112443318542a8da67a8
Carlos Diaz-Padron <carlos@metaphor.xyz>
2023-05-05 11:47:19 -0400

    add defense level context

--- brain/src/subsystems/overlord.ts
+++ brain/src/subsystems/overlord.ts
@@ -17,6 +17,7 @@ Time Left in Game: 14 days
 - You should ask subordinate functions for recommendations of action and capabilities available before issuing orders.
 - You need to conduct research that takes time in order to increase capabilities.
 - As time passes, it's good to check in on status of resource collection, research, and human activity on the island
+- Increasing your defense level will also reduce resource gathering rate. You should adjust the defense level up and down accordingly.
 
 Writing Style: Short, all caps, robotic
 
